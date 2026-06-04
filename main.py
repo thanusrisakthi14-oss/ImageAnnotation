c16ebdf0f2d3d66b01a191df10b62450b98756e4
thanusrisakthi14-oss <thanusrisakthi14@gmail.com>
2026-06-04 19:55:52 +0100

    .

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import cv2
 import matplotlib.pyplot as plt
 image = cv2.imread("baby_2.jpg")
 image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-cv2.rectangle(image, (20, 20), (170, 170), (0, 255, 255), 3)
+cv2.rectangle(image, (334, 20), (500, 240), (0, 255, 255), 3)
 cv2.circle(image, (100, 100), 15, (0, 255, 255), 3)
 cv2.line(image, (20, 20), (170, 170), (0, 255, 255), 3)
-cv2.putText(image,"Baby",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),2)
+cv2.putText(image,"Baby",(334,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),2)
 plt.imshow(image)
 plt.show()
